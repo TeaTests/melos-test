5b05de5d2e7271d0fdd839ac609f0c3d3e5d4dbc
rahul-2901 <rahul.6@brightlifecare.com>
2025-06-07 19:44:34 +0530

    feat: add two numbers

--- packages/packageone/lib/packageone.dart
+++ packages/packageone/lib/packageone.dart
@@ -4,4 +4,7 @@ library packageone;
 class Addition {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+
+  /// Returns [first] plus [second].
+  int add(int first, int second) => first + second;
 }
