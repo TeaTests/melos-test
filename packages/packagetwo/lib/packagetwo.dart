930545123ac06e3416b3abdad1b5fe3e3cc72087
rahul-2901 <rahul.6@brightlifecare.com>
2025-06-07 19:38:19 +0530

    feat: added addition + multiplication support

--- packages/packagetwo/lib/packagetwo.dart
+++ packages/packagetwo/lib/packagetwo.dart
@@ -1,7 +1,13 @@
 library packagetwo;
 
+import 'package:packageone/packageone.dart';
+
 /// A Calculator.
 class Multiplication {
+  final Addition addition = Addition();
+
   /// Returns [value] added to itself [times] time.
   int multiply(int value, int times) => value * times;
+
+  int multipleAddition(int value, int times, int add) => addition.addOne(value * times);
 }
