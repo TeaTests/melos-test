9c312e8ec5d728cc6406fa9f8afa0b585ecf00f0
rahul-2901 <rahul.6@brightlifecare.com>
2025-06-07 19:01:20 +0530

    refactor!: updated dart package name to Multiplication

--- packages/packagetwo/test/packagetwo_test.dart
+++ packages/packagetwo/test/packagetwo_test.dart
@@ -4,7 +4,7 @@ import 'package:packagetwo/packagetwo.dart';
 
 void main() {
   test('multiply two numbers', () {
-    final calculator = Calculator();
+    final calculator = Multiplication();
     expect(calculator.multiply(2, 1), 2);
     expect(calculator.multiply(2, 2), 4);
     expect(calculator.multiply(2, -1), -2);
