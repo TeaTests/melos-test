9c6b0ef3dcc44ced30667c4e5a8f581ffdde8693
rahul-2901 <rahul.6@brightlifecare.com>
2025-06-07 19:14:48 +0530

    refactor!: updated class package name

--- packages/packagetwo/lib/packagetwo.dart
+++ packages/packagetwo/lib/packagetwo.dart
@@ -1,7 +1,7 @@
 library packagetwo;
 
 /// A Calculator.
-class Calculator {
+class Multiplication {
   /// Returns [value] added to itself [times] time.
   int multiply(int value, int times) => value * times;
 }
