0a6e327158ba37dd4055b8a7201e4e744a24c0da
rahul-2901 <rahul.6@brightlifecare.com>
2025-06-07 19:57:46 +0530

    feat: added additon, multiplication support

--- packages/calc/lib/calc.dart
+++ packages/calc/lib/calc.dart
@@ -1,7 +1,14 @@
 library calc;
 
+import 'package:packageone/packageone.dart';
+import 'package:packagetwo/packagetwo.dart';
+
 /// A Calculator.
 class Calculator {
+  final Addition addition = Addition();
+  final Multiplication multiplication = Multiplication();
+
   /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  int increment(int value) => addition.addOne(value);
+  int multiply(int value, int times) => multiplication.multiply(value, times);
 }
