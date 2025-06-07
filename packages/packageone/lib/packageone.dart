35d3662aa16f2abed55742229db752e6200291a0
rahul-2901 <rahul.6@brightlifecare.com>
2025-06-07 19:15:15 +0530

    refactor!: updated class package name

--- packages/packageone/lib/packageone.dart
+++ packages/packageone/lib/packageone.dart
@@ -1,7 +1,7 @@
 library packageone;
 
 /// A Calculator.
-class Calculator {
+class Addition {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
--- packages/packageone/test/packageone_test.dart
+++ packages/packageone/test/packageone_test.dart
@@ -4,7 +4,7 @@ import 'package:packageone/packageone.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
+    final calculator = Addition();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
